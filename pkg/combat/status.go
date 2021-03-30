03641de3dd0dd53c8276360eea00373e642f9555
srliao <906239+srliao@users.noreply.github.com>
2021-03-30 18:33:52 -0400

    sync

--- pkg/combat/status.go
+++ pkg/combat/status.go
@@ -1,15 +1,5 @@
 package combat
 
-func (s *Sim) decrementStatusDuration() {
-	for k, v := range s.Status {
-		if v == 0 {
-			delete(s.Status, k)
-		} else {
-			s.Status[k]--
-		}
-	}
-}
-
 func (s *Sim) StatusActive(key string) bool {
 	return s.Status[key] >= s.F
 }
