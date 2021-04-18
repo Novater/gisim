f69da8898f36a0aa5a85aab1cbf3c135b0ecbe6f
srliao <906239+srliao@users.noreply.github.com>
2021-04-18 16:24:56 -0400

    add multi iteration mode

--- README.md
+++ README.md
@@ -2,77 +2,20 @@
 
 ## instructions
 
-download Go for your operating system
-
-clone the repo; `cd gisim`
-
-either `go build` and then run the executable (in command line) or just use `go run main.go`
-
-Flags:
-
-| flag | acceptable            | info                                                                                                                                   |
-| ---- | --------------------- | -------------------------------------------------------------------------------------------------------------------------------------- |
-| d    | `debug` `info` `warn` | level out log output, defaults to `warn`                                                                                               |
-| p    | `whatever.yaml`       | config file to use; defaults to `config.yaml`                                                                                          |
-| s    | any number            | number representing seconds to run sim for, defaults to `600`                                                                          |
-| o    | any string            | log file to write to; if blank no log. defaults to no log                                                                              |
-| hp   | any number            | force sim to run in hp mode instead; default runs in time mode. if in hp mode sim will run until the specified damage has been reached |
-| c    | `true/false`          | whether or not print caller function in debug log                                                                                      |
-
-Example `go run main.go -d=debug -s=20`
-
-To save the log to file (easier to read) `go run main.go -d=debug -o=out.log`
+See [wiki](https://github.com/srliao/gisim/wiki)
 
 ## cpu profiling
 
 CPU profilling is enabled by default for optimization purposes. If you wish to check it out, run ` go tool pprof -pdf cpu.pprof >| file.pdf` after running the sim (best run the sim for a longer period of time to generate useful cpu profile)
 
-## bugs/issues
+## todo priority list
 
-- there should be an animation lock if normal attacks are not chained
-- sim assumes A4 is available even if lvl specified has not unlocked A4
-- not sure how to implement Amos bow yet? maybe add post arrow fire hook? [could just add an initial frame to snapshot, then diff between that and current frame would be travel time]
-- current overload formula seems to give higher damage than actual; actual 2482, got 2594.3517401129357
-- we don't track self auras. this is a problem for swirl w elemental absorption
-- swirl EC is incorrect; currently only trigger swirlelectro but need to trigger both; need to restructure reacitontype to take an array... but that becomes a mess :(
-- anemo resonance reduced CD not yet implemented
-- atkspd implementation is probably wrong.. can't find any good info on this :(
-
-## todo list
-
-- [ ] clean up debug output
-- [ ] clean up documentation
 - [ ] play test xl/xq/ben/fish w/ basic weapons
 - [ ] change normal reset to a frame number and if s.F = frame, then reset
 - [ ] rand artifact code is bugged
 - [ ] change runEffects back to a map; loops can break if one effect adds another effect
 - [ ] jump/dash/char switch/burst/skill force reset all char normal counter
 - [ ] frames returned per action should have at least 2 number, avg cancellable and avg normal; may actually required more than 2, 1 into each trailling action such as swap, dash, jump, burst, skill, auto
-
-## rotation syntax ideas
-
-action list example
-
-```
-actions+=sequence_strict target=xingqiu exec=skill,burst lock=100;
-actions+=skill target=xingqiu if=.status.xingqiu.energy<80 lock=100;
-actions+=burst target=xingqiu;
-actions+=burst target=bennett;
-actions+=sequence_strict target=xiangling exec=skill,burst;
-actions+=skill target=xiangling active=xiangling;
-actions+=burst target=fischl if=.status.xiangling.energy<70&&.tags.fischl.oz==0 swap=xiangling;
-actions+=skill target=fischl if=.status.xiangling.energy<70&&.tags.fischl.oz==0 swap=xiangling;
-actions+=burst target=fischl if=.tags.fischl.oz==0;
-actions+=skill target=fischl if=.tags.fischl.oz==0;
-actions+=skill target=bennett if=.status.xiangling.energy<40 swap=xiangling;
-actions+=skill target=bennett;
-actions+=attack target=xiangling;
-actions+=attack target=xingqiu active=xingqiu;
-actions+=attack target=bennett active=bennett;
-actions+=attack target=fischl active=fischl;
-```
-
-`sequence` and `sequence_rest` not yet properly implemented
 
 ## optimizations
 
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -50,34 +51,88 @@ import (
 
 func main() {
 	var source []byte
-	var cfg combat.Profile
+
 	var err error
 
-	debugPtr := flag.String("d", "error", "output level: debug, info, warn")
-	secondsPtr := flag.Int("s", 600, "how many seconds to run the sim for")
-	pPtr := flag.String("p", "config.yaml", "which profile to use")
+	debug := flag.String("d", "error", "output level: debug, info, warn")
+	seconds := flag.Int("s", 120, "how many seconds to run the sim for")
+	cfgFile := flag.String("p", "config.yaml", "which profile to use")
 	f := flag.String("o", "", "detailed log file")
 	hp := flag.Float64("hp", 0, "hp mode: how much hp to deal damage to")
 	showCaller := flag.Bool("c", false, "show caller in debug low")
-	flag.Parse()
+	avgMode := flag.Bool("a", false, "run sim multiple times and calculate avg damage (smooth out randomness). default false. note that there is no debug log in this mode")
+	w := flag.Int("w", 24, "number of workers to run when running multiple iterations; default 24")
+	i := flag.Int("i", 200, "number of iterations to run if we're running multiple")
 
-	defer profile.Start(profile.ProfilePath("./")).Stop()
+	flag.Parse()
 
 	// p := "./xl-base.yaml" //xl.yaml expecting 4659 dps
 
-	source, err = ioutil.ReadFile(*pPtr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = yaml.Unmarshal(source, &cfg)
+	source, err = ioutil.ReadFile(*cfgFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cfg.LogConfig.LogLevel = *debugPtr
-	cfg.LogConfig.LogFile = *f
-	cfg.LogConfig.LogShowCaller = *showCaller
-	os.Remove(*f)
+	if *avgMode {
+		runIter(*i, *w, source, *hp, *seconds)
+	} else {
+		defer profile.Start(profile.ProfilePath("./")).Stop()
+		var cfg combat.Profile
+		err = yaml.Unmarshal(source, &cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cfg.LogConfig.LogLevel = *debug
+		cfg.LogConfig.LogFile = *f
+		cfg.LogConfig.LogShowCaller = *showCaller
+		os.Remove(*f)
+		runSingle(cfg, *hp, *seconds)
+	}
+
+}
+
+func graphToCSV(in []float64) {
+	os.Remove("graph.csv")
+	file, err := os.Create("result.csv")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	t := 10
+	l := len(in) / (60 * t)
+
+	out := make([][]string, 0, l)
+
+	stop := false
+	next := 0
+	var prev float64
+	var i int64
+	for !stop {
+		val := in[next] - prev
+		prev = in[next]
+		out = append(out, []string{strconv.FormatInt(i, 10), strconv.FormatFloat(val/float64(t), 'f', 2, 64)})
+		i++
+		if next == len(in)-1 {
+			stop = true
+		} else {
+			next += 600
+			if next >= len(in) {
+				next = len(in) - 1
+			}
+		}
+	}
+
+	for _, v := range out {
+		writer.Write(v)
+	}
+
+}
+
+func runSingle(cfg combat.Profile, hp float64, dur int) {
 
 	s, err := combat.New(cfg)
 	if err != nil {
@@ -87,13 +142,12 @@ func main() {
 	var stats combat.SimStats
 	var dmg float64
 
-	dur := *secondsPtr
 	start := time.Now()
-	if *hp > 0 {
-		dmg, stats = s.RunHPMode(*hp)
+	if hp > 0 {
+		dmg, stats = s.RunHPMode(hp)
 		dur = stats.SimDuration / 60
 	} else {
-		dmg, stats = s.Run(*secondsPtr)
+		dmg, stats = s.Run(dur)
 	}
 	elapsed := time.Since(start)
 	fmt.Println("------------------------------------------")
@@ -154,50 +208,107 @@ func main() {
 		fmt.Printf("%v: %v\n", k, v)
 	}
 	fmt.Println("------------------------------------------")
-	fmt.Printf("Running profile %v, total damage dealt: %.2f over %v seconds. DPS = %.2f. Sim took %s\n", *pPtr, dmg, dur, dmg/float64(dur), elapsed)
+	fmt.Printf("Running profile %v, total damage dealt: %.2f over %v seconds. DPS = %.2f. Sim took %s\n", cfg.Label, dmg, dur, dmg/float64(dur), elapsed)
 
-	if *hp == 0 {
+	if hp == 0 {
 		graphToCSV(stats.DamageHist)
 	}
-
 }
 
-func graphToCSV(in []float64) {
-	os.Remove("graph.csv")
-	file, err := os.Create("result.csv")
-	if err != nil {
-		log.Panic(err)
+func runIter(n, w int, src []byte, hp float64, dur int) {
+	var progress, sum, ss, min, max float64
+	var data []float64
+	min = math.MaxFloat64
+	max = -1
+
+	count := n
+
+	resp := make(chan float64, n)
+	req := make(chan bool)
+	done := make(chan bool)
+
+	for i := 0; i < w; i++ {
+		go worker(src, hp, dur, resp, req, done)
 	}
-	defer file.Close()
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-	t := 10
-	l := len(in) / (60 * t)
+	go func() {
+		var wip int
+		for wip < n {
+			req <- true
+			wip++
+		}
+	}()
 
-	out := make([][]string, 0, l)
+	fmt.Print("\tProgress: 0")
 
-	stop := false
-	next := 0
-	var prev float64
-	var i int64
-	for !stop {
-		val := in[next] - prev
-		prev = in[next]
-		out = append(out, []string{strconv.FormatInt(i, 10), strconv.FormatFloat(val/float64(t), 'f', 2, 64)})
-		i++
-		if next == len(in)-1 {
-			stop = true
-		} else {
-			next += 600
-			if next >= len(in) {
-				next = len(in) - 1
-			}
+	for count > 0 {
+		val := <-resp
+		count--
+		data = append(data, val)
+		sum += val
+		if val < min {
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+		if (1 - float64(count)/float64(n)) > (progress + 0.1) {
+			progress = (1 - float64(count)/float64(n))
+			fmt.Printf(".%.0f", 100*progress)
 		}
 	}
+	fmt.Print("...done\n")
 
-	for _, v := range out {
-		writer.Write(v)
+	close(done)
+
+	mean := sum / float64(n)
+
+	for _, v := range data {
+		ss += (v - mean) * (v - mean)
 	}
 
+	sd := math.Sqrt(ss / float64(n))
+
+	fmt.Printf(
+		"Simulation done; %v iterations resulted in average dps of %0.0f over %v seconds (min: %0.02f, max: %0.02f, stddev: %0.02f)\n",
+		n,
+		mean,
+		dur,
+		min,
+		max,
+		sd,
+	)
+}
+
+func worker(src []byte, hp float64, dur int, resp chan float64, req chan bool, done chan bool) {
+
+	for {
+		select {
+		case <-req:
+			var cfg combat.Profile
+			err := yaml.Unmarshal(src, &cfg)
+			if err != nil {
+				log.Fatal(err)
+			}
+			cfg.LogConfig.LogLevel = "error"
+			cfg.LogConfig.LogFile = ""
+			cfg.LogConfig.LogShowCaller = false
+
+			s, err := combat.New(cfg)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if hp > 0 {
+				dmg, _ := s.RunHPMode(hp)
+				resp <- dmg / float64(dur)
+			} else {
+				dmg, _ := s.Run(dur)
+				resp <- dmg / float64(dur)
+			}
+
+		case <-done:
+			return
+		}
+	}
 }
